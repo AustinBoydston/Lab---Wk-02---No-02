35def6aef27094ad53bb0930da7405afb0468ff7
AustinBoydston <austin.t.boydston-1@ou.edu>
2020-02-22 23:31:09 -0600

    Finalize

--- ascii.cpp
+++ ascii.cpp
@@ -1,19 +1,459 @@
 #include <iostream>
 using namespace std;
 
-int main()
+
+void A();
+void B();
+void C();
+
+void D();
+void E();
+void F();
+
+void G();
+void H();
+void I();
+
+void J();
+void K();
+void L();
+
+void M();
+void N();
+void O();
+
+void P();
+void Q();
+void R();
+
+void S();
+void T();
+void U();
+
+void V();
+void W();
+void X();
+
+void Y();
+void Z();
+void OSU();
+
+int main(int num, char* input1[])
+{
+ char* temp = input1[2];
+switch(*temp)
+{
+case 'A': A();
+return 1;
+break;
+case 'B': B();
+return 2;
+break;
+case 'C': C();
+return 3;
+break;
+case 'D': D();
+return 4;
+break;
+case 'E': E();
+return 5;
+break;
+case 'F': F();
+return 6;
+break;
+case 'G': G();
+return 7;
+break;
+case 'H': H();
+return 8;
+break;
+case 'I': I();
+return 9;
+break;
+case 'J': J();
+return 10;
+break;
+case 'K': K();
+return 11;
+break;
+case 'L': L();
+return 12;
+break;
+case 'M': M();
+return 13;
+break;
+case 'N': N();
+return 14;
+break;
+case 'O': O();
+return 15;
+break;
+case 'P': P();
+return 16;
+break;
+case 'Q': Q();
+return 17;
+break;
+case 'R': R();
+return 18;
+break;
+case 'S': S();
+return 19;
+break;
+case 'T': T();
+return 20;
+break;
+case 'U': U();
+return 21;
+break;
+case 'V': V();
+return 22;
+break;
+case 'W': W();
+return 23;
+break;
+case 'X': X();
+return 24;
+break;
+case 'Y': Y();
+return 25;
+break;
+case 'Z': Z();
+return 26;
+break;
+
+default: OSU(); 
+}
+return 0;
+}
+
+/////////////////////////////////////////////Method Implementation/////////////////////////////////////
+
+ void A()
+{
+cout << "     **    " << endl;
+cout << "    ****   " << endl;
+cout << "   **//**  " << endl;
+cout << "  **  //** " << endl;
+cout << " ********** " <<  endl;
+cout << "/**//////**" << endl;
+cout << "/**     /** "<<  endl;
+cout << "//      // " << endl;
+}
+
+ void B()
+{
+cout << " ****** " << endl;  
+cout << "/*////** " << endl; 
+cout << "/*   /** " << endl; 
+cout << "/******  " << endl; 
+cout << "/*//// **" << endl;
+cout << "/*    /**" << endl;
+cout << "/*******" << endl; 
+cout << "///////  " << endl;
+}
+
+ void C()
+{
+cout << "   ****** " << endl;
+cout << "  **////**" << endl;
+cout << " **    // " << endl;
+cout << "/**       " << endl;
+cout << "/**       " << endl;
+cout << "//**    **" << endl;
+cout <<" //****** "  << endl;
+cout << "  //////  " << endl;
+}
+
+ void D()
+{
+cout << " ******* " << endl; 
+cout <<"/**////** " << endl;
+cout <<"/**    /**" << endl;
+cout <<"/**    /**" << endl;
+cout <<"/**    /**" << endl;
+cout <<"/**    ** "<< endl;
+cout <<"/*******"  << endl;
+cout <<"/////// "  << endl;
+}
+
+ void E()
+{
+cout <<" ********"<< endl;
+cout <<"/**///// "<< endl;
+cout <<"/**      "<< endl;
+cout <<"/*******" << endl;
+cout <<"/**////  "<< endl;
+cout <<"/**      "<< endl;
+cout <<"/********"<< endl;
+cout <<"//////// "<< endl;
+}
+
+ void F()
+{
+cout <<" ********"<< endl;
+cout <<"/**///// "<< endl;
+cout <<"/**      "<< endl;
+cout <<"/******* "<< endl;
+cout <<"/**////  "<< endl;
+cout <<"/**     " << endl;
+cout <<"/**    " << endl;
+cout <<"//    "<< endl;
+}
+
+ void G()
+{
+cout <<"   ******** "<< endl;
+cout <<"  **//////** "<< endl;
+cout <<" **      // "<< endl;
+cout <<"/**         "<< endl;
+cout <<"/**    ***** "<< endl;
+cout <<"//**  ////** "<< endl;
+cout <<" //******** "<< endl;
+cout <<"  //////// " << endl;
+}
+
+ void H()
+{
+cout <<" **      **"<< endl;
+cout <<"/**     /**"<< endl;
+cout <<"/**     /**"<< endl;
+cout <<"/**********"<< endl;
+cout <<"/**//////** "<< endl;
+cout <<"/**     /** "<< endl;
+cout <<"/**     /** "<< endl;
+cout <<"//      // " << endl;
+}
+
+ void I()
+{
+cout <<" **"<< endl;
+cout <<"/**"<< endl;
+cout <<"/** "<< endl;
+cout <<"/** "<< endl;
+cout <<"/** "<< endl;
+cout <<"/** "<< endl;
+cout <<"/** "<< endl;
+cout <<"// " << endl;
+}
+
+ void J()
+{
+cout <<"      **"<< endl;
+cout <<"     /**"<< endl;
+cout <<"     /** "<< endl;
+cout <<"     /** "<< endl;
+cout <<"     /** "<< endl;
+cout <<" **  /** "<< endl;
+cout <<"//***** "<< endl;
+cout <<" ///// " << endl;
+}
+
+ void K()
+{
+cout <<" **   **"<< endl;
+cout <<"/**  ** "<< endl;
+cout <<"/** ** "<< endl;
+cout <<"/****"<< endl;
+cout <<"/**/**"<< endl;
+cout <<"/**//**"<< endl;
+cout <<"/** //**"<< endl;
+cout <<"//   // " << endl;
+}
+
+ void L()
+{
+cout <<" **"<< endl;
+cout <<"/** "<< endl;
+cout <<"/**  "<< endl;
+cout <<"/**"<< endl;
+cout <<"/**"<< endl;
+cout <<"/**"<< endl;
+cout <<"/*******"<< endl;
+cout <<"/////// " << endl;
+}
+
+ void M()
+{
+cout <<" ****     ****"<< endl;
+cout <<"/**/**   **/**"<< endl;
+cout <<"/**//** ** /**"<< endl;
+cout <<"/** //***  /**"<< endl;
+cout <<"/**  //*   /**"<< endl;
+cout <<"/**   /    /**"<< endl;
+cout <<"/**        /**" << endl;
+cout <<"//         // "<< endl;
+}
+
+ void N()
+{
+cout <<" ****     **"<< endl;
+cout <<"/**/**   /**"<< endl;
+cout <<"/**//**  /**"<< endl;
+cout <<"/** //** /**"<< endl;
+cout <<"/**  //**/**"<< endl;
+cout <<"/**   //****"<< endl;
+cout <<"/**    //***"<< endl;
+cout <<"//      /// " << endl;
+}
+
+ void O()
+{
+cout <<"   *******  "<< endl;
+cout <<"  **/////**"<< endl;
+cout <<" **     //** "<< endl;
+cout <<"/**      /**"<< endl;
+cout <<"/**      /**"<< endl;
+cout <<"//**     **"<< endl;
+cout <<" //*******"<< endl;
+cout <<"  /////// " << endl;
+}
+
+ void P()
+{
+cout <<" *******"<< endl;
+cout <<"/**////**"<< endl;
+cout <<"/**   /**"<< endl;
+cout <<"/*******"<< endl;
+cout <<"/**////"<< endl;
+cout <<"/** "<< endl;
+cout <<"/** "<< endl;
+cout <<"// " << endl;
+}
+
+ void Q()
+{
+cout <<"   *******  "<< endl;
+cout <<"  **/////** "<< endl;
+cout <<" **     //**"<< endl;
+cout <<"/**      /**"<< endl;
+cout <<"/**    **/**"<< endl;
+cout <<"//**  // **"<< endl;
+cout <<" //******* **"<< endl;
+cout <<"  /////// //" << endl;
+}
+
+ void R()
+{
+cout <<" ******* "<< endl;
+cout <<"/**////**"<< endl;
+cout <<"/**   /**"<< endl;
+cout <<"/******* "<< endl;
+cout <<"/**///** "<< endl;
+cout <<"/**  //**"<< endl;
+cout <<"/**   //**"<< endl;
+cout <<"//     //" << endl;
+}
+
+ void S()
+{
+cout <<"  ********"<< endl;
+cout <<" **//////"<< endl;
+cout <<"/** "<< endl;
+cout <<"/*********"<< endl;
+cout <<"////////**"<< endl;
+cout <<"       /**"<< endl;
+cout <<" ******** "<< endl;
+cout <<"//////// " << endl;
+}
+void T()
+{
+cout <<" **********"<< endl;
+cout <<"/////**/// "<< endl;
+cout <<"    /**   "<< endl;
+cout <<"    /**   "<< endl;
+cout <<"    /**   "<< endl;
+cout <<"    /**   "<< endl;
+cout <<"    /**   "<< endl;
+cout <<"    //   " << endl;
+}
+
+ void U()
+{
+cout <<" **     **"<< endl;
+cout <<"/**    /**"<< endl;
+cout <<"/**    /**"<< endl;
+cout <<"/**    /**"<< endl;
+cout <<"/**    /**"<< endl;
+cout <<"/**    /**"<< endl;
+cout <<"//*******"<< endl;
+cout <<" /////// " << endl;
+}
+
+ void V()
+{
+    cout <<" **      **"<< endl;
+cout <<"/**     /**"<< endl;
+cout <<"/**     /**"<< endl;
+cout <<"//**    ** "<< endl;
+cout <<" //**  **"<< endl;
+cout <<"  //****"<< endl;
+cout <<"   //** "<< endl;
+cout <<"    //   " << endl;
+}
+ void W()
+{
+cout <<" **       **"<< endl;
+cout <<"/**      /**"<< endl;
+cout <<"/**   *  /**"<< endl;
+cout <<"/**  *** /**"<< endl;
+cout <<"/** **/**/**"<< endl;
+cout <<"/**** //****"<< endl;
+cout <<"/**/   ///** "<< endl;
+cout <<"//       // " << endl;
+}
+
+ void X()
+{
+cout <<" **     **"<< endl;
+cout <<"//**   ** "<< endl;
+cout <<" //** **   "<< endl;
+cout <<"  //***   "<< endl;
+cout <<"   **/**  "<< endl;
+cout <<"  ** //**  "<< endl;
+cout <<" **   //**"<< endl;
+cout <<"//     // " << endl;
+}
+
+ void Y()
+{
+    cout <<" **    **"<< endl;
+cout <<"//**  **"<< endl;
+cout <<" //**** "<< endl;
+cout <<"  //**"<< endl;
+cout <<"   /** "<< endl;
+cout <<"   /** "<< endl;
+cout <<"   /**  "<< endl;
+cout <<"   // " << endl;
+}
+
+ void Z()
+{
+cout <<" ********"<< endl;
+cout <<"//////** "<< endl;
+cout <<"     ** "<< endl;
+cout <<"    **  "<< endl;
+cout <<"   **  "<< endl;
+cout <<"  **   "<< endl;
+cout <<" ********"<< endl;
+cout <<"////////" << endl;
+}
+
+
+
+
+void OSU()
 {
 
-cout <<
-"   *******    ******** **     **"
- << endl <<" **/////**  **////// /**    /**"
-<< endl <<" **     //**/**       /**    /**"
-<< endl <<"/**      /**/*********/**    /**"
-<< endl <<"/**      /**////////**/**    /**"
-<< endl <<"//**     **        /**/**    /**"
-<< endl <<" //*******   ******** //******* "
-<< endl <<"  ///////   ////////   ///////  "
-<< endl;
-return 0;
+cout <<"   *******    ******** **     **" << endl;
+ cout << " **/////**  **////// /**    /**"<< endl;
+cout <<" **     //**/**       /**    /**"<< endl;
+cout <<"/**      /**/*********/**    /**"<< endl;
+cout <<"/**      /**////////**/**    /**"<< endl;
+cout <<"//**     **        /**/**    /**"<< endl;
+cout <<" //*******   ******** //******* "<< endl;
+cout <<"  ///////   ////////   ///////  " << endl;
 
 }
+
+
